e5c55fb1fe270f2b823ac4625a067c6cc5964de6
Andrei Komolov <andreykomolov@gmail.com>
2020-09-13 17:18:58 -0400

    Ädd Email SignUp

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import  { FirebaseContext } from '../Firebase';
+
 const Account = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
+  <FirebaseContext.Consumer>
+    {firebase => {
+      return <div>I've access to Firebase and render something.</div>;
+    }}
+  </FirebaseContext.Consumer>
 );
 
 export default Account;
