3d422dafc6b6b2c5725336b9a5c20bf465c4880e
Andrei Komolov <andreykomolov@gmail.com>
2020-09-14 16:11:58 -0400

    Reset password component

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,18 +1,19 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 
-const config = {
+const firebaseConfig = {
     apiKey: "AIzaSyBYStn4zYgQUkAmhB819Rsq8kmKKUdvJQw",
     authDomain: "fir-app-2b040.firebaseapp.com",
     databaseURL: "fir-app-2b040.firebaseio.com",
     projectId: "fir-app-2b040",
-    storageBucket: "gs://fir-app-2b040.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    storageBucket: "fir-app-2b040.appspot.com",
+    messagingSenderId: "650172937676",
+    appId: "1:650172937676:web:ad8b94453243e3e25f9969",
 };
 
 class Firebase {
     constructor() {
-        app.initializeApp(config);
+        app.initializeApp(firebaseConfig);
 
         this.auth = app.auth();
     }
